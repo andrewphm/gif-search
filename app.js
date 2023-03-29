d3eb52e2fc347ac5ff473357d0b591c93809fee4
Andrew Pham <andrew.pham@shopify.com>
2023-03-29 17:16:17 -0400

    Add search form

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  const gifUrl =
-    'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245';
-
-  res.render('hello-gif', { gifUrl: gifUrl });
+  res.render('home');
 });
 
 app.get('/greetings/:name', (req, res) => {
