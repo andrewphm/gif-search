8c08b628c71e6c67ad8a730bb8016b7f1eeca532
Andrew Pham <andrew.pham@shopify.com>
2023-03-29 17:30:56 -0400

    Added working gif search

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const Tenor = require('tenorjs').client({
+  // Replace with your own key
+  Key: 'AIzaSyAe4vYsft7-FWEU9v-wU4wjUtUp6SwzP1k', // https://tenor.com/developer/keyregistration
+  Filter: 'high', // "off", "low", "medium", "high", not case sensitive
+  Locale: 'en_US', // Your locale here, case-sensitivity depends on input
+});
 
 // Middleware
 const exphbs = require('express-handlebars');
@@ -8,7 +14,18 @@ app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.render('home');
+  term = '';
+
+  if (req.query.term) {
+    term = req.query.term;
+  }
+
+  Tenor.Search.Query(term, '10')
+    .then((response) => {
+      const gifs = response;
+      res.render('home', { gifs });
+    })
+    .catch(console.error);
 });
 
 app.get('/greetings/:name', (req, res) => {
